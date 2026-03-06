8d415397bc033faa31c53db3c282fa485a437e2f
spencershore <t.spencer.shore@gmail.com>
2026-03-06 11:42:51 -0500

    Displays usage when given single argument

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,15 @@
 int main(int argc, char* argv[]) {
   std::vector<std::string> filecontents;
 
+  if (argc == 1) {
+    std::cout << "usage: ecrypt [-v | --version] [-h | --help]" << std::endl;
+    std::cout << "              <command> [<args>]" << std::endl;
+    std::cout << std::endl;
+    std::cout << "Encrypt a file" << std::endl;
+    std::cout << "\t[-e | encrypt] <file>                  \t\tEncrypt a file with default encryption settings" << std::endl;
+    std::cout << "\t[-d | decrypt] <decryption-type> <file>\t\tDecrypt a file with the given decryption type" << std::endl;
+  }
+
   if (argc == 2) {
     filecontents = readFromFile(argv[1]);
   }
