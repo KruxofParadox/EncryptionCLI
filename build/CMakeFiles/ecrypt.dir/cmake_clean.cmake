ddfe37f13ac540177d55b1cc00ed838cc31f7d66
spencershore <t.spencer.shore@gmail.com>
2026-03-11 18:51:53 -0400

    Added cmake implementation for project

---
+++ build/CMakeFiles/ecrypt.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ecrypt.dir/src/main.cpp.obj"
+  "CMakeFiles/ecrypt.dir/src/main.cpp.obj.d"
+  "ecrypt.exe"
+  "ecrypt.exe.manifest"
+  "ecrypt.pdb"
+  "libecrypt.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ecrypt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
