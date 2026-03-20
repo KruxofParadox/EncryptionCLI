ebb1bf33a1fd750b308c5ac4353591437e51f799
spencershore <t.spencer.shore@gmail.com>
2026-03-20 12:36:35 -0400

    Added test cases for caesarCipher

--- tests/test.cpp
+++ tests/test.cpp
@@ -12,3 +12,13 @@ TEST_CASE("SplitString with comma delimiter", "[splitString]") {
   REQUIRE(splitString("comma,separated,values", ',') == std::vector<std::string>{"comma", "separated", "values"});
 }
 
+TEST_CASE("Caesar Cipher") {
+  std::vector<std::string> test1 = {"aaaa"};
+  std::vector<std::string> test2 = {"test test test"};
+  runCaesarCipher(&test1, 1);
+  runCaesarCipher(&test2, 2);
+
+  REQUIRE(test1 == std::vector<std::string>{"bbbb"});
+  REQUIRE(test2 == std::vector<std::string>{"vguv vguv vguv"});
+}
+
